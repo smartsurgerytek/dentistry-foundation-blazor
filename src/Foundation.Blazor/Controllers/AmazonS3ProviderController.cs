9dc3d29007631e03326af9b752add84fb6932135
sulaythakkar27 <sulay.thakkar@faciletechnolab.com>
2025-02-19 15:42:12 +0530

    added file management system using sync fusion file manager control

---
+++ src/Foundation.Blazor/Controllers/AmazonS3ProviderController.cs
@@ -0,0 +1,137 @@
+﻿using Syncfusion.EJ2.FileManager.AmazonS3FileProvider;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using Syncfusion.EJ2.FileManager.Base;
+using Amazon;
+using System.Text.Json;
+
+namespace EJ2AmazonS3ASPCoreFileProvider.Controllers
+{
+
+    [Route("api/[controller]")]
+    [EnableCors("AllowAllOrigins")]
+    public class AmazonS3ProviderController : Controller
+    {
+        public AmazonS3FileProvider operation;
+        public string basePath;
+        protected RegionEndpoint bucketRegion;
+        public AmazonS3ProviderController(IWebHostEnvironment hostingEnvironment)
+        {
+            this.basePath = hostingEnvironment.ContentRootPath;
+            this.basePath = basePath.Replace("../", "");
+            this.operation = new AmazonS3FileProvider();
+            this.operation.RegisterAmazonS3("smartsurgerytek.foundation", "AKIAZI2LGNNVDTFYF57P", "tR/1EYOayK8i5R5DCZTJCyqAXCkDVMJWYhYEfDRp", "us-west-2");
+        }
+        [Route("AmazonS3FileOperations")]
+        public object AmazonS3FileOperations([FromBody] FileManagerDirectoryContent args)
+        {
+            if (args.Action == "delete" || args.Action == "rename")
+            {
+                if ((args.TargetPath == null) && (args.Path == ""))
+                {
+                    FileManagerResponse response = new FileManagerResponse();
+                    ErrorDetails er = new ErrorDetails
+                    {
+                        Code = "401",
+                        Message = "Restricted to modify the root folder."
+                    };
+                    response.Error = er;
+                    return this.operation.ToCamelCase(response);
+                }
+            }
+            switch (args.Action)
+            {
+                case "read":
+                    // reads the file(s) or folder(s) from the given path.
+                    return this.operation.ToCamelCase(this.operation.GetFiles(args.Path, false, args.Data));
+                case "delete":
+                    // deletes the selected file(s) or folder(s) from the given path.
+                    return this.operation.ToCamelCase(this.operation.Delete(args.Path, args.Names, args.Data));
+                case "copy":
+                    // copies the selected file(s) or folder(s) from a path and then pastes them into a given target path.
+                    return this.operation.ToCamelCase(this.operation.Copy(args.Path, args.TargetPath, args.Names, args.RenameFiles, args.TargetData, args.Data));
+                case "move":
+                    // cuts the selected file(s) or folder(s) from a path and then pastes them into a given target path.
+                    return this.operation.ToCamelCase(this.operation.Move(args.Path, args.TargetPath, args.Names, args.RenameFiles, args.TargetData, args.Data));
+                case "details":
+                    // gets the details of the selected file(s) or folder(s).
+                    return this.operation.ToCamelCase(this.operation.Details(args.Path, args.Names, args.Data));
+                case "create":
+                    // creates a new folder in a given path.
+                    return this.operation.ToCamelCase(this.operation.Create(args.Path, args.Name, args.Data));
+                case "search":
+                    // gets the list of file(s) or folder(s) from a given path based on the searched key string.
+                    return this.operation.ToCamelCase(this.operation.Search(args.Path, args.SearchString, args.ShowHiddenItems, args.CaseSensitive, args.Data));
+                case "rename":
+                    // renames a file or folder.
+                    return this.operation.ToCamelCase(this.operation.Rename(args.Path, args.Name, args.NewName, false, args.ShowFileExtension, args.Data));
+            }
+            return null;
+        }
+
+        // uploads the file(s) into a specified path
+        [Route("AmazonS3Upload")]
+        public IActionResult AmazonS3Upload(string path, IList<IFormFile> uploadFiles, string action, string data)
+        {
+            FileManagerResponse uploadResponse;
+            FileManagerDirectoryContent[] dataObject = new FileManagerDirectoryContent[1];
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            };
+            dataObject[0] = JsonSerializer.Deserialize<FileManagerDirectoryContent>(data, options);
+            foreach (var file in uploadFiles)
+            {
+                var folders = (file.FileName).Split('/');
+                // checking the folder upload
+                if (folders.Length > 1)
+                {
+                    for (var i = 0; i < folders.Length - 1; i++)
+                    {
+                        if (!this.operation.checkFileExist(path, folders[i]))
+                        {
+                            this.operation.ToCamelCase(this.operation.Create(path, folders[i], dataObject));
+                        }
+                        path += folders[i] + "/";
+                    }
+                }
+            }
+            int chunkIndex = int.TryParse(HttpContext.Request.Form["chunk-index"], out int parsedChunkIndex) ? parsedChunkIndex : 0;
+            int totalChunk = int.TryParse(HttpContext.Request.Form["total-chunk"], out int parsedTotalChunk) ? parsedTotalChunk : 0;
+            uploadResponse = operation.Upload(path, uploadFiles, action, dataObject, chunkIndex, totalChunk);
+            if (uploadResponse.Error != null)
+            {
+                Response.Clear();
+                Response.ContentType = "application/json; charset=utf-8";
+                Response.StatusCode = Convert.ToInt32(uploadResponse.Error.Code);
+                Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = uploadResponse.Error.Message;
+            }
+            return Content("");
+        }
+
+        // downloads the selected file(s) and folder(s)
+        [Route("AmazonS3Download")]
+        public IActionResult AmazonS3Download(string downloadInput)
+        {
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            };
+            FileManagerDirectoryContent args = JsonSerializer.Deserialize<FileManagerDirectoryContent>(downloadInput, options);
+            return operation.Download(args.Path, args.Names);
+        }
+
+        // gets the image(s) from the given path
+        [Route("AmazonS3GetImage")]
+        public IActionResult AmazonS3GetImage(FileManagerDirectoryContent args)
+        {
+            return operation.GetImage(args.Path, args.Id, false, null, args.Data);
+        }
+    }
+
+}
