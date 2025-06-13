2f685968e8229c90a25b465a4f08bb85150e8698
sulaythakkar27 <sulay.thakkar@faciletechnolab.com>
2025-06-13 15:14:14 +0000

    undoing changes

--- src/Foundation.Blazor.Client/Pages/Index.razor.cs
+++ src/Foundation.Blazor.Client/Pages/Index.razor.cs
@@ -12,15 +12,15 @@ public partial class Index
         Logger.LogInformation($"Is current user authenticated: {CurrentUser.IsAuthenticated}");
         Logger.LogInformation($"Current user roles: {string.Join(", ", CurrentUser.Roles)}");
         Logger.LogInformation($"Current user name: {CurrentUser.Name}");
-        // if (CurrentUser.IsAuthenticated && CurrentUser.Roles.Contains("Doctor"))
-        // {
-        //     await Task.Delay(2000); // Simulate some delay for better UX
-        //     NavigationManager.NavigateTo("/Patient");
-        // }
-        // else
-        // {
-        //     await Task.Delay(5000); // Simulate some delay for better UX
-        //     NavigationManager.NavigateTo($"/authentication/login", true);
-        // }
+        if (CurrentUser.IsAuthenticated && CurrentUser.Roles.Contains("Doctor"))
+        {
+            await Task.Delay(1000); // Simulate some delay for better UX
+            NavigationManager.NavigateTo("/Patient");
+        }
+        else
+        {
+            await Task.Delay(5000); // Simulate some delay for better UX
+            NavigationManager.NavigateTo($"/authentication/login", true);
+        }
     }
 }
