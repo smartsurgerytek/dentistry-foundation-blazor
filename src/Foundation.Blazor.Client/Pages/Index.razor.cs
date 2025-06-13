e5afedebe361b00b69d26847e7be72d3acd25f7f
sulaythakkar27 <sulay.thakkar@faciletechnolab.com>
2025-06-13 12:22:37 +0000

    changes for resolving deployment issues

--- src/Foundation.Blazor.Client/Pages/Index.razor.cs
+++ src/Foundation.Blazor.Client/Pages/Index.razor.cs
@@ -8,6 +8,9 @@ public partial class Index
 {
     protected override Task OnInitializedAsync()
     {
+        Console.WriteLine($"Is current user authenticated: {CurrentUser.IsAuthenticated}");
+        Console.WriteLine($"Current user roles: {string.Join(", ", CurrentUser.Roles)}");
+        Console.WriteLine($"Current user name: {CurrentUser.Name}");
         if (CurrentUser.IsAuthenticated && CurrentUser.Roles.Contains("Doctor"))
         {
             NavigationManager.NavigateTo("/Patient");
