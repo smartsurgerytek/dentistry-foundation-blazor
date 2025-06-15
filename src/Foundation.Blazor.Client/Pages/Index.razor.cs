d4f04c9dc168a89ddc88f851ca9360fb36c654c9
sulaythakkar27 <sulay.thakkar@faciletechnolab.com>
2025-06-15 13:35:58 +0000

    no authentication for now

--- src/Foundation.Blazor.Client/Pages/Index.razor.cs
+++ src/Foundation.Blazor.Client/Pages/Index.razor.cs
@@ -9,20 +9,22 @@ public partial class Index
 {
     protected override async Task OnInitializedAsync()
     {
-        var authState = await AuthStateProvider.GetAuthenticationStateAsync();
-        var user = authState.User;
-        Logger.LogInformation($"User identity is authenticated: {user.Identity?.IsAuthenticated ?? false}");
-        Logger.LogInformation($"User identity name: {user.Identity?.Name ?? "Anonymous"}");
+        // var authState = await AuthStateProvider.GetAuthenticationStateAsync();
+        // var user = authState.User;
+        // Logger.LogInformation($"User identity is authenticated: {user.Identity?.IsAuthenticated ?? false}");
+        // Logger.LogInformation($"User identity name: {user.Identity?.Name ?? "Anonymous"}");
 
-        if (!user.Identity?.IsAuthenticated ?? true)
-        {
-            await Task.Delay(5000); // Simulate some delay for better UX
-            NavigationManager.NavigateTo($"/authentication/login", true);
-        }
-        else
-        {
-            await Task.Delay(1000); // Simulate some delay for better UX
-            NavigationManager.NavigateTo("/Patient");
-        }
+        // if (!user.Identity?.IsAuthenticated ?? true)
+        // {
+        //     await Task.Delay(500); // Simulate some delay for better UX
+        //     NavigationManager.NavigateTo($"/authentication/login", true);
+        // }
+        // else
+        // {
+        //     await Task.Delay(500); // Simulate some delay for better UX
+        //     NavigationManager.NavigateTo("/Patient");
+        // }
+
+        NavigationManager.NavigateTo("/Patient", true);
     }
 }
